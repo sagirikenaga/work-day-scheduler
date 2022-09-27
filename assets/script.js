c70b0e0105602fa818c0b8f32ab359d468d54639
Sagiri <sagiri@Sagiris-MBP.hitronhub.home>
2022-09-27 10:19:17 -0400

    added colour coat for time blocks

--- assets/script.js
+++ assets/script.js
@@ -4,28 +4,24 @@ $("#current-date").text(date);
 function colourCoat() {
     // Sets interval in variable
     var dailyInterval = setInterval(function() {
-        var currentTime = moment().hours(); 
-        console.log(currentTime);
+        // var currentTime = moment().hours(); 
+        var currentTime = 15;
 
-      for(let id = 9; id <= 17;id++){
-            console.log(id);
-           let block =  document.getElementById(JSON.stringify(id))
-           console.log(block)
-  
-      if(id < currentTime) { 
-        //past event
-        document.getElementById("#id").className="past";
-      }
-      //current event
-      if(id = currentTime) {
-        document.getElementById("#id").className="present";
-      }
-      if (id > currentTime) {
-        document.getElementById("#id").className="future";
-      }
+      for (let id = 9; id <= 17; id++) {
+
+        if (id < currentTime) { 
+            //past event
+            document.getElementById(id).className="past";
+        }
+            //current event
+            else if(id == currentTime) {
+            document.getElementById(id).className="present";
+        }
+            else if (id > currentTime) {
+            document.getElementById(id).className="future";
+        }
+
     }
-
-  
     }, 1000);
   }
 
