1c9435b72f9673c1bfe96bfc3a7205b7faf462a2
Sagiri <sagiri@Sagiris-MBP.hitronhub.home>
2022-10-19 19:20:39 -0400

    more changes, acceptance criteria met

--- assets/script.js
+++ assets/script.js
@@ -4,21 +4,20 @@ $("#current-date").text(date);
 function colourCoat() {
     // Sets interval in variable
     var dailyInterval = setInterval(function() {
-        // var currentTime = moment().hours(); 
-        var currentTime = 15;
+        var currentTime = moment().hours(); 
 
       for (let id = 9; id <= 17; id++) {
 
         if (id < currentTime) { 
             //past event
-            document.getElementById(id).className="past";
+            document.getElementById(id).className="past description";
         }
             //current event
             else if(id == currentTime) {
-            document.getElementById(id).className="present";
+            document.getElementById(id).className="present description";
         }
             else if (id > currentTime) {
-            document.getElementById(id).className="future";
+            document.getElementById(id).className="future description";
         }
 
     }
@@ -27,4 +26,26 @@ function colourCoat() {
 
   colourCoat();
 
+let ls = JSON.parse(window.localStorage.getItem("event"));
 
+$(".save").on("click", function () {
+    // get nearby values
+    let value = $(this).siblings("textarea").val();
+    let hour = $(this).siblings("textarea").attr("id");
+    // save in localStorage
+    // localStorage.setItem(time, value);
+    let todos = JSON.parse(window.localStorage.getItem("event")) || [];
+    console.log(todos);
+    todos.push({hour, value});
+    localStorage.setItem("event", JSON.stringify(todos));
+});
+
+$("textarea").each(function(index) {
+    let timeBlock = $(this).attr('id');
+    for (let i = 0; i < ls.length; i++) {
+        const element = ls[i];
+        if (timeBlock === element.hour) {
+            $(this).text(element.value);
+        }
+    }
+  });
